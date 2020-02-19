31db178e3e32e4a5fab6c369fdf3e4c46619ad6f
Brandon Weaver <a7xncob@gmail.com>
2020-02-19 21:32:17 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,4 +1,5 @@
 const counter = document.getElementById("counter");
+
 const plusButton = document.getElementById("plus");
 const minusButton = document.getElementById("minus");
 const pauseButton = document.getElementById("pause");
